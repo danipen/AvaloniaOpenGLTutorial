bdf0144ecc6d2bf61274615f3907855e5754afa6
Daniel Peñalba <dpenalba@codicesoftware.com>
2022-09-12 16:14:30 +0200

    Cleanup code
    - Do not use aPos now, since it's not needed to configure gl_Position, it's 0,0,0 by default.
    - Remove gl.CheckError and do it once per method only (now that all is working as expected)

--- tutorial2/MainWindow.axaml.cs
+++ tutorial2/MainWindow.axaml.cs
@@ -30,46 +30,32 @@ namespace Tutorial2
 
                 ConfigureShader(gl);
                 CreateVertexBuffer(gl);
+                
+                gl.CheckError();
             }
 
             void ConfigureShader(GlInterface gl)
             {
-
                 mShaderProgram = gl.CreateProgram();
-                gl.CheckError();
 
                 CreateVertexShader(gl);
                 CreateVertexFragmentShader(gl);
 
                 Console.WriteLine(gl.LinkProgramAndGetError(mShaderProgram));
-                gl.CheckError();
             }
 
             void CreateVertexFragmentShader(GlInterface gl)
             {
                 mVertexFragmentShader = gl.CreateShader(GL_FRAGMENT_SHADER);
-                gl.CheckError();
-
                 Console.WriteLine(gl.CompileShaderAndGetError(mVertexFragmentShader, VertexFragmentShaderSource));
-                gl.CheckError();
-
                 gl.AttachShader(mShaderProgram, mVertexFragmentShader);
-                gl.CheckError();
             }
 
             void CreateVertexShader(GlInterface gl)
             {
                 mVertexShader = gl.CreateShader(GL_VERTEX_SHADER);
-                gl.CheckError();
-
                 Console.WriteLine(gl.CompileShaderAndGetError(mVertexShader, VertexShaderSource));
-                gl.CheckError();
-
                 gl.AttachShader(mShaderProgram, mVertexShader);
-                gl.CheckError();
-
-                gl.BindAttribLocationString(mShaderProgram, 0, "aPos");
-                gl.CheckError();
             }
 
             protected override void OnOpenGlDeinit(GlInterface gl, int fb)
@@ -85,19 +71,10 @@ namespace Tutorial2
                 gl.Viewport(0, 0, (int)Bounds.Width, (int)Bounds.Height);
 
                 gl.UseProgram(mShaderProgram);
-                gl.CheckError();
-
                 gl.BindBuffer(GL_ARRAY_BUFFER, mVBO);
-                gl.CheckError();
-
                 gl.BindVertexArray(mVAO);
-                gl.CheckError();
-
                 gl.EnableVertexAttribArray(0);
-                gl.CheckError();
-
                 gl.VertexAttribPointer(0,3, GL_FLOAT, GL_FALSE, 0,IntPtr.Zero);
-                gl.CheckError();
 
                 gl.DrawArrays(GL_POINTS, 0, new IntPtr(1));
                 gl.CheckError();
@@ -110,45 +87,36 @@ namespace Tutorial2
 
                 mVBO = gl.GenBuffer();
                 gl.BindBuffer(GL_ARRAY_BUFFER, mVBO);
-                gl.CheckError();
-
                 gl.BufferData(GL_ARRAY_BUFFER, new IntPtr(sizeof(Vector3)),
                         new IntPtr(&vertex), GL_STATIC_DRAW);
-                gl.CheckError();
 
                 mVAO = gl.GenVertexArray();
                 gl.BindVertexArray(mVAO);
-                gl.CheckError();
+
 
                 gl.VertexAttribPointer(
                     0, 3, GL_FLOAT, 0, 0, IntPtr.Zero);
-                gl.CheckError();
-
                 gl.EnableVertexAttribArray(0);
-                gl.CheckError();
             }
 
 
-            string VertexShaderSource => GlExtensions.GetShader(GlVersion, false, @"
-       vec3 aPos;
-  
-        out vec4 vertexColor;
+            string VertexShaderSource => GlExtensions.GetShader(GlVersion, false, @" 
+                out vec4 vertexColor;
 
-        void main()
-        {
-            gl_Position = vec4(aPos, 1.0);
-            vertexColor = vec4(1.0, 1.0, 1.0, 1.0);
-        }
+                void main()
+                {
+                    vertexColor = vec4(1.0, 1.0, 1.0, 1.0);
+                }
 ");
             string VertexFragmentShaderSource => GlExtensions.GetShader(GlVersion, true, @"
-        out vec4 FragColor;
-          
-        in vec4 vertexColor; // the input variable from the vertex shader (same name and same type)  
+                out vec4 FragColor;
+              
+                in vec4 vertexColor; // the input variable from the vertex shader (same name and same type)  
 
-        void main()
-        {
-            FragColor = vertexColor;
-        } 
+                void main()
+                {
+                    FragColor = vertexColor;
+                } 
 ");
 
             int mVBO;
