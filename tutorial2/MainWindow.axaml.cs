0a4a088c85116a9a72f2b63633b91a8f43259272
Augusto Nalin <augusto.nalin@unity3d.com>
2022-09-12 14:41:01 +0100

    added shaders
    shaders are needed to be able to draw on MacOS

--- tutorial2/MainWindow.axaml.cs
+++ tutorial2/MainWindow.axaml.cs
@@ -1,8 +1,7 @@
 using System;
 using System.Numerics;
+
 using Avalonia.Controls;
-using Avalonia.Input;
-using Avalonia.Interactivity;
 using Avalonia.OpenGL;
 using Avalonia.OpenGL.Controls;
 using static Avalonia.OpenGL.GlConsts;
@@ -18,13 +17,10 @@ namespace Tutorial2
             InitializeComponent();
 
             MyOpenGlControl myControl = new MyOpenGlControl();
-
             this.Content = myControl;
-
-
         }
 
-        class MyOpenGlControl : OpenGlControlBase
+        unsafe class MyOpenGlControl : OpenGlControlBase
         {
             protected override void OnOpenGlInit(GlInterface gl, int fb)
             {
@@ -32,8 +28,48 @@ namespace Tutorial2
 
                 gl.ClearColor(0,0,0,0);
 
+                ConfigureShader(gl);
                 CreateVertexBuffer(gl);
-                GlExtensions.CheckError(gl);
+            }
+
+            void ConfigureShader(GlInterface gl)
+            {
+
+                mShaderProgram = gl.CreateProgram();
+                gl.CheckError();
+
+                CreateVertexShader(gl);
+                CreateVertexFragmentShader(gl);
+
+                Console.WriteLine(gl.LinkProgramAndGetError(mShaderProgram));
+                gl.CheckError();
+            }
+
+            void CreateVertexFragmentShader(GlInterface gl)
+            {
+                mVertexFragmentShader = gl.CreateShader(GL_FRAGMENT_SHADER);
+                gl.CheckError();
+
+                Console.WriteLine(gl.CompileShaderAndGetError(mVertexFragmentShader, VertexFragmentShaderSource));
+                gl.CheckError();
+
+                gl.AttachShader(mShaderProgram, mVertexFragmentShader);
+                gl.CheckError();
+            }
+
+            void CreateVertexShader(GlInterface gl)
+            {
+                mVertexShader = gl.CreateShader(GL_VERTEX_SHADER);
+                gl.CheckError();
+
+                Console.WriteLine(gl.CompileShaderAndGetError(mVertexShader, VertexShaderSource));
+                gl.CheckError();
+
+                gl.AttachShader(mShaderProgram, mVertexShader);
+                gl.CheckError();
+
+                gl.BindAttribLocationString(mShaderProgram, 0, "aPos");
+                gl.CheckError();
             }
 
             protected override void OnOpenGlDeinit(GlInterface gl, int fb)
@@ -41,66 +77,85 @@ namespace Tutorial2
                 base.OnOpenGlDeinit(gl, fb);
             }
 
-            protected unsafe override void OnOpenGlRender(GlInterface gl, int fb)
+            protected override void OnOpenGlRender(GlInterface gl, int fb)
             {
+                gl.ClearColor(0, 0, 0, 1);
                 gl.Clear( GL_COLOR_BUFFER_BIT);
-                GlExtensions.CheckError(gl);
 
-                gl.BindBuffer(GL_ARRAY_BUFFER, VBO);
-                GlExtensions.CheckError(gl);
+                gl.Viewport(0, 0, (int)Bounds.Width, (int)Bounds.Height);
+
+                gl.UseProgram(mShaderProgram);
+                gl.CheckError();
+
+                gl.BindBuffer(GL_ARRAY_BUFFER, mVBO);
+                gl.CheckError();
+
+                gl.BindVertexArray(mVAO);
+                gl.CheckError();
 
                 gl.EnableVertexAttribArray(0);
-                GlExtensions.CheckError(gl);
+                gl.CheckError();
 
                 gl.VertexAttribPointer(0,3, GL_FLOAT, GL_FALSE, 0,IntPtr.Zero);
-                GlExtensions.CheckError(gl);
+                gl.CheckError();
 
-                var count = 1;
-                gl.DrawArrays(GL_POINTS, 0, new IntPtr(&count));
-                GlExtensions.CheckError(gl);
-
-                // glClear(GL_COLOR_BUFFER_BIT);
-                //
-                // glBindBuffer(GL_ARRAY_BUFFER, VBO);
-                //
-                // glEnableVertexAttribArray(0);
-                //
-                // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-                //
-                // glDrawArrays(GL_POINTS, 0, 1);
-                //
-                // glDisableVertexAttribArray(0);
-                //
-                // glutSwapBuffers();
+                gl.DrawArrays(GL_POINTS, 0, new IntPtr(1));
+                gl.CheckError();
             }
 
 
-            unsafe void CreateVertexBuffer(GlInterface gl)
+            void CreateVertexBuffer(GlInterface gl)
             {
-                Vector3 vertex = new Vector3(0,0,0);
-                //VBO = new IntPtr(&vertex);
-                //int* VBOData = (int *)VBO.ToPointer();
+                Vector3 vertex = new Vector3(0, 0, 0);
 
-                VBO = gl.GenBuffer();
-                gl.BindBuffer(GL_ARRAY_BUFFER, VBO);
+                mVBO = gl.GenBuffer();
+                gl.BindBuffer(GL_ARRAY_BUFFER, mVBO);
+                gl.CheckError();
 
-                int size = sizeof(Vector3);
-                gl.BufferData(GL_ARRAY_BUFFER, new IntPtr(&size), new IntPtr(&vertex), GL_STATIC_DRAW);
+                gl.BufferData(GL_ARRAY_BUFFER, new IntPtr(sizeof(Vector3)),
+                        new IntPtr(&vertex), GL_STATIC_DRAW);
+                gl.CheckError();
 
-                VAO = gl.GenVertexArray();
-                gl.BindVertexArray(VAO);
+                mVAO = gl.GenVertexArray();
+                gl.BindVertexArray(mVAO);
+                gl.CheckError();
 
-                // Vector3f Vertices[1];
-                // Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
-                //
-                // glGenBuffers(1, &VBO);
-                // glBindBuffer(GL_ARRAY_BUFFER, VBO);
-                // glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+                gl.VertexAttribPointer(
+                    0, 3, GL_FLOAT, 0, 0, IntPtr.Zero);
+                gl.CheckError();
+
+                gl.EnableVertexAttribArray(0);
+                gl.CheckError();
             }
 
-            private int VBO;
-            private int VAO;
 
+            string VertexShaderSource => GlExtensions.GetShader(GlVersion, false, @"
+       vec3 aPos;
+  
+        out vec4 vertexColor;
+
+        void main()
+        {
+            gl_Position = vec4(aPos, 1.0);
+            vertexColor = vec4(1.0, 1.0, 1.0, 1.0);
+        }
+");
+            string VertexFragmentShaderSource => GlExtensions.GetShader(GlVersion, true, @"
+        out vec4 FragColor;
+          
+        in vec4 vertexColor; // the input variable from the vertex shader (same name and same type)  
+
+        void main()
+        {
+            FragColor = vertexColor;
+        } 
+");
+
+            int mVBO;
+            int mVAO;
+            int mVertexShader;
+            int mVertexFragmentShader;
+            int mShaderProgram;
         }
     }
 }
