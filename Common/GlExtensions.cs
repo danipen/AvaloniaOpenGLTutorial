a752ca4b037503c898475300afd4b560057bf698
Daniel Peñalba <dpenalba@codicesoftware.com>
2022-09-12 13:31:39 +0200

    Enable a way to visualize GL_ERRORS

---
+++ Common/GlExtensions.cs
@@ -0,0 +1,15 @@
+using Avalonia.OpenGL;
+
+using static Avalonia.OpenGL.GlConsts;
+
+namespace Avalonia.OpenGL;
+
+public static class GlExtensions
+{
+    public static void CheckError(this GlInterface gl)
+    {
+        int err;
+        while ((err = gl.GetError()) != GL_NO_ERROR)
+            Console.WriteLine(err);
+    }
+}
