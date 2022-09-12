8c87eb4f33d91562adcc27fe0917a8fe4db85776
Daniel Peñalba <dpenalba@codicesoftware.com>
2022-09-12 10:02:26 +0200

    First project structure for tutorial 1

---
+++ tutorial1/MainWindow.axaml.cs
@@ -0,0 +1,39 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.OpenGL;
+using Avalonia.OpenGL.Controls;
+
+namespace tutorial1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MyOpenGlControl myControl = new MyOpenGlControl();
+
+            this.Content = myControl;
+        }
+
+        class MyOpenGlControl : OpenGlControlBase
+        {
+            protected override void OnOpenGlInit(GlInterface gl, int fb)
+            {
+                base.OnOpenGlInit(gl, fb);
+            }
+
+            protected override void OnOpenGlDeinit(GlInterface gl, int fb)
+            {
+                base.OnOpenGlDeinit(gl, fb);
+            }
+
+            protected override void OnOpenGlRender(GlInterface gl, int fb)
+            {
+                
+            }
+        }
+    }
+}
