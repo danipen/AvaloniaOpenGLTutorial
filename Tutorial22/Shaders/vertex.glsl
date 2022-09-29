7b49d474e5cf03cc006e5ff76d5ebd3392fc386c
Augusto Nalin <augusto.nalin@unity3d.com>
2022-09-29 18:36:53 +0100

    added mesh transform to fragment shader
    this allows for granular control how transform is applied to each vertex or normal

--- Tutorial22/Shaders/vertex.glsl
+++ Tutorial22/Shaders/vertex.glsl
@@ -4,13 +4,15 @@ in vec3 normal;
 
 uniform mat4 gLocalTransform;
 uniform mat4 gWorldTransform;
+uniform mat4 gMeshTransform;
 
 out vec2 texCoord0;
 out vec3 normal0;
 
 void main()
 {
-    gl_Position = gLocalTransform * vec4(position, 1.0);
+    gl_Position = gLocalTransform * gMeshTransform * vec4(position, 1.0);
     texCoord0 = texCoord;
-    normal0 = (gWorldTransform * vec4(normal, 0.0)).xyz;
+    mat3 norm_matrix = transpose(inverse(mat3(gWorldTransform * gMeshTransform)));
+    normal0 = normalize(norm_matrix * normal);
 }
